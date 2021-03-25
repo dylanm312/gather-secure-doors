a532d7632195fd2501650bee5ac1cc1b3e084fb2
Dylan Moreland <dylan.c.moreland@gmail.com>
2021-03-24 18:28:33 -0700

    Add models and register in admin site

--- gather_secure_doors/door_management/admin.py
+++ gather_secure_doors/door_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Workspace, Room, Door
 
 # Register your models here.
+admin.site.register(Workspace)
+admin.site.register(Room)
+admin.site.register(Door)
--- gather_secure_doors/door_management/models.py
+++ gather_secure_doors/door_management/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Workspace(models.Model):
+    workspace_name = models.CharField(max_length=200)
+    workspace_id = models.CharField(max_length=200)
+    api_key = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.workspace_name
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=200)
+    room_id = models.CharField(max_length=200)
+    workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.room_name
+
+class Door(models.Model):
+    door_name = models.CharField(max_length=200)
+    open_image = models.ImageField()
+    closed_image = models.ImageField()
+    width = models.IntegerField(default=1)
+    height = models.IntegerField(default=2)
+    x_position = models.IntegerField(default=0)
+    y_position = models.IntegerField(default=0)
+    password = models.CharField(max_length=200)
+    door_url = models.URLField(max_length=200)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.door_name
